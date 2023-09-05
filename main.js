e5b9941d487c6ddd29d736f89b3df3e6c986a184
Yuri Farias <yurisoaresf@gmail.com>
2023-09-05 10:00:54 -0300

    API Rest Project

---
+++ main.js
@@ -0,0 +1,71 @@
+//Variáveis(constantes) para importar as bibliotecas necessárias pro projeto.
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const express = require("express");
+
+//Variável(constante) demonstando como será o esquema do dado a ser salvo(json).
+const FilmeSchema = {
+    titulo: String,
+    genero: String,
+    ano: Number
+}
+const Filme = mongoose.model("Filme", FilmeSchema);
+//Criação de uma variavel(constante), que dentro dela terá uma arrow function, que será asyn, necessaria para poder utilizar a syntax do await, e evitar os callbacks de then(), catch, etc.
+const funcaoAsyncParaUsarOAwait = async() => {
+
+    //Variáveis para determinar que o modelo de dado a ser salvo e recuperado, será o que foi determinado anteriormente pela const FilmeSchema (Json).
+    // const Filme = mongoose.model("Filme", FilmeSchema);
+
+    //Conectando-se ao banco de dados, utilizando o mongoose, será possivel conectar ao MongoDB, e utilizar as operaçoes de CRUD que ja vem junto com o framework Mongoose.
+    console.log("Conectando-se ao banco de dados...");
+    await mongoose.connect("mongodb://127.0.0.1/ProjetoServidorWeb");
+    console.log("Estou conectado!");
+
+    //Dado que será salvo no banco, aqui está sendo mockado, mas é possivel por exemplo, através dos metodos de API Rest que ainda será criado com ajuda do framework Express, será possível salvar um dado que virá do Postman, por exemplo.
+    await Filme({titulo: "Senhor dos Anéis", genero: "Fantasia", ano: 2001}).save();
+
+
+    //Variaveis para buscar os determinados filmes salvos.
+    let procuraFilme = await Filme.find({});
+
+    //let deleteFilme = await Filme.deleteMany({});
+
+    //Exibição dos filmes encontrados.
+    console.log(procuraFilme);
+
+    //console.log(deleteFilme);
+
+};
+
+funcaoAsyncParaUsarOAwait();
+
+//Inicio da criaçao do Servidor Web, criando variável(constante) para dizer que utilizarmos a partir de agora, o Express, tudo será chamado através do app.servidor web, com o express.
+// const Filme = mongoose.model("Filme", FilmeSchema);
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Fazendo a arquitetura de API Rest para manter recurso Filmes:
+app.get("/filmes", async(req,res) => {
+    const filmeExistente = await Filme.find({})
+    res.json(filmeExistente);
+
+});
+
+app.post("/filmesSalvar", async(req, res) => {
+    const filmesSalvos = await Filme(req.body).save()
+    res.json(filmesSalvos);
+});
+
+console.log("Iniciando o Servidor Web...");
+app.listen(3000, () => {
+    console.log("Servidor está no ar!");
+    console.log("Acesse em http://127.0.0.1:3000");
+});
+
+
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Este readme apenas relata o que foi usado neste projeto, como:
+
+MongoDB - Banco de dados NoSQL
+Express - Framework utilizado pra criaçao de um servidor web, assim como, poder importar todas as funções e métodos necessários para integração de API Rest, com todos os metodos HTTP, etc.
+Nodemon - Espécie de plugin (Lembra o LiveServer) que faz com que toda modificação feita no projeto, seja atualizada em tempo real, sem precisar parar a aplicação e rodar novamente com o npm start por exemplo.
+Mongoose - Framework estilo JPA, só que para Node.js, ou seja, ele tem todos os métodos CRUD para voce realizar com o banco MongoDB.
+Body-Parser - Transforma todo dado que vem de frontend, em objetos(req.body).
